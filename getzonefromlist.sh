9636db6197b5a94f0b0462e0b94b366b0a6c9b0f
Rial Sloan II <rial.sloan.ii@gmail.com>
2016-10-03 22:15:25 -0400

    variables added to protect the guilty

--- getzonefromlist.sh
+++ getzonefromlist.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 # USAGE ./getzonefromlist.sh LISTOFDOMAINS TARGETDIRECTORYFORZONEFILES
+$server='axfr.server.zone.tld'
+
+#
+#
+#
 
 mkdir $2
-awk '{print $1}' $1 | while read domain; do dig @ns1.cha.smartechcorp.net -t axfr +multiline +noadditional +onesoa +noquestion +nostats +nocmd $domain >> $2/$domain.zone; done;
+awk '{print $1}' $1 | while read domain; do dig @$server -t axfr +multiline +noadditional +onesoa +noquestion +nostats +nocmd $domain >> $2/$domain.zone; done;
